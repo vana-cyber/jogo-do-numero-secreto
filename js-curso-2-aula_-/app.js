39aa975910e85055147276d8a79ff528fa8c9e6a
Geovana Frota <geovanafrota2019@gmail.com>
2024-08-24 20:36:19 -0300

    alterando o valor pra 100

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -5,7 +5,7 @@
 //paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
 let listaNumeroSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -16,7 +16,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemIncial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número de 1 a 10');
+    exibirTextoNaTela('p', 'Escolha um número de 1 a 100');
 }
 
 exibirMensagemIncial();
